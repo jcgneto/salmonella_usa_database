16c1252c841c9ec24a998cb4adb5bb82d9652c0c
Joao Carlos Gomes Neto <jgomesneto2@unl.edu>
2019-07-01 15:59:02 -0500

    Update file

--- scripts/trim.sh
+++ scripts/trim.sh
@@ -28,4 +28,3 @@ do
    R2unpaired=${R2//.fastq/_unpaired.fastq.gz}	
    trimmomatic PE -threads 12 $R1 $R2 $R1paired $R1unpaired $R2paired $R2unpaired CROP:215 LEADING:10 TRAILING:10 SLIDINGWINDOW:5:20 MINLEN:215
 done
-
