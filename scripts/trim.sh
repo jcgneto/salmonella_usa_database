5e2fd6f85ec51049cc2f061c01b4a64486eaf212
Joao Carlos Gomes Neto <jgomesneto2@unl.edu>
2019-07-15 15:56:40 -0500

    Update trim.sh

--- scripts/trim.sh
+++ scripts/trim.sh
@@ -26,5 +26,5 @@ do
    R1unpaired=${R1//.fastq/_unpaired.fastq.gz}	
    R2paired=${R2//.fastq/_paired.fastq.gz}
    R2unpaired=${R2//.fastq/_unpaired.fastq.gz}	
-   trimmomatic PE -threads 12 $R1 $R2 $R1paired $R1unpaired $R2paired $R2unpaired HEADCROP:15 CROP:215 LEADING:10 TRAILING:10 SLIDINGWINDOW:5:20 MINLEN:50
+   trimmomatic PE -threads 12 $R1 $R2 $R1paired $R1unpaired $R2paired $R2unpaired HEADCROP:15 CROP:200 LEADING:10 TRAILING:10 SLIDINGWINDOW:5:20 MINLEN:50
 done
