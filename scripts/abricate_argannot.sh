57eefdaed125a89cfb6246d7404c442d50cb4b7d
Joao Carlos Gomes Neto <jgomesneto2@unl.edu>
2019-07-30 23:57:24 -0500

    Create abricate_argannot.sh

---
+++ scripts/abricate_argannot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --partition=benson
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --time=72:00:00
+#SBATCH --mem=100GB
+#SBATCH --job-name="abricate_argannot"
+#SBATCH --error="abricate_argannot.%J.error"
+#SBATCH --output="abricate_argannot.%J.out"
+
+#module load
+
+module load blast/2.7
+module load abricate/0.8 
+
+#enter the directory
+
+DATAIN=/work/benson/netogomes/pop_genomics/salmonella_project_1/data_raw_files/paper_1_analysis/abricate/abricate_argannot
+
+cd $DATAIN
+
+abricate --db argannot *.fa --csv
