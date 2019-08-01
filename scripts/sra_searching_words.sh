0bb6d8f6d491109f1f787b2c6154151ea972d071
Joao Carlos Gomes Neto <jgomesneto2@unl.edu>
2019-08-01 15:07:03 -0500

    Create sra_searching_words.sh

---
+++ scripts/sra_searching_words.sh
@@ -0,0 +1,4 @@
+(("Salmonella enterica"[Organism] OR ("Salmonella enterica"[Organism] 
+OR ("Salmonella enterica"[Organism] OR salmonella enterica[All Fields]))) 
+AND (cluster_public[prop] AND "biomol dna"[Properties] 
+AND "strategy wgs"[Properties]) AND "Illumina"[All Fields]) AND "USA"[All Fields]
