5071494992cb82339acc0ca952f6b768cfa90eae
Joao Carlos Gomes Neto <jgomesneto2@unl.edu>
2019-08-08 16:49:51 -0500

    Update fasttree.sh

--- scripts/fasttree.sh
+++ scripts/fasttree.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+#SBATCH --partition=benson
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --mem=100GB
+#SBATCH --job-name="fasttree"
+#SBATCH --error="fasttree.%J.error"
+#SBATCH --output="fasttree.%J.out"
+
+#module load
+
+module load fasttree/2.1
+
+DATAIN=
+DATAOUT=
+
+cd $DATAIN
+
 FastTree -nt -pseudo -fastest < core_gene_alignment.aln > core_gene_alignment.tree
