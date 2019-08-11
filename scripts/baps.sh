b8854a7da6a540ec46b3b7a2047c6a0dc96ffa55
Joao Carlos Gomes Neto <jgomesneto2@unl.edu>
2019-08-11 10:01:13 -0500

    Update baps.sh

--- scripts/baps.sh
+++ scripts/baps.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #SBATCH --partition=benson
 #SBATCH --time=72:00:00
 #SBATCH --nodes=1
@@ -11,13 +12,31 @@
 #module load
 
 module load anaconda/4.6
-module load raxml/8.2
-module load fasttree/2.1
-module load biopython/py36/1.71
-module load gubbins/2.3
+conda create -n r-rhierbaps r-rhierbaps
+conda activate r-rhierbaps
 
-DATAIN=/work/benson/netogomes/pop_genomics/salmonella_project_1/data_raw_files/paper_1_analysis/prokka_human_bovine/prokka_output/roary/gubbins
+DATAIN=/work/benson/netogomes/pop_genomics/salmonella_project_1/data_raw_files/paper_1_analysis/prokka_human_bovine/prokka_output/roary/baps 
 
 cd $DATAIN
 
-run_gubbins.py -t fasttree -i 100 -m 3 -v core_gene_alignment.aln
+Rscript --vanilla baps.R
+
+
+# the baps.R is a separate script although I am putting them together here
+
+# Load library
+
+library("rhierbaps")
+
+# Set seed to reproduce results
+
+set.seed(1234)
+
+# Load data
+
+input_file <- system.file("extdata", "core_gene_alignment.fa", package = "rhierbaps")
+snp_matrix <- load_fasta(input_file)
+hb_results <- hierBAPS(snp_matrix, max.depth = 2, n.pops = 50, n.extra.rounds = Inf, 
+    quiet = TRUE)
+write.csv(hb_results$partition.df, file = file.path(/work/benson/netogomes/pop_genomics/salmonella_project_1/data_raw_files/paper_1_analysis/prokka_human_bovine/prokka_output/roary/baps, "hierbaps_partition.csv"), 
+    col.names = TRUE, row.names = FALSE)
