9e824d473611ef205b997ce985f0203e997ede99
Joao Carlos Gomes Neto <jgomesneto2@unl.edu>
2019-08-28 21:26:37 -0500

    Update baps.sh

--- scripts/baps.sh
+++ scripts/baps.sh
@@ -29,10 +29,7 @@ library("rhierbaps")
 set.seed(1234)
 
 # Load data
-# input_file <- system.file("extdata", "core_gene_alignment.fa", package = "rhierbaps")
 input_file <- ape::read.dna("core_gene_alignment.fa", format="fasta")
 snp_matrix <- load_fasta(input_file)
 hb_results <- hierBAPS(snp_matrix, max.depth = 1, n.pops = 150, n.extra.rounds = Inf, quiet = TRUE)
 write.csv(hb_results$partition.df, file = file.path("/work/benson/netogomes/pop_genomics/salmonella_project_1/data_raw_files/paper_1_analysis/prokka_human_bovine/prokka_output/roary/baps", "hierbaps_partition.csv"), col.names = TRUE, row.names = FALSE)
-#hb_results_prob <- hierBAPS(snp_matrix, max.depth = 1, n.pops = 50, n.extra.rounds = Inf, quiet = TRUE, assignment.probs = TRUE)
-#write.csv(hb_results_prob$cluster.assignment.prob[[1]], file = file.path("/work/benson/netogomes/pop_genomics/salmonella_project_1/data_raw_files/paper_1_analysis/prokka_human_bovine/prokka_output/roary/baps", "hierbaps_probabilities.csv")
