b70b20549623f50bd6c69aacfa2fd3ca9f1ef709
Joao Carlos Gomes Neto <jgomesneto2@unl.edu>
2019-10-12 18:04:56 -0500

    Delete sra_searching_words.sh

--- scripts/sra_searching_words.sh
+++
@@ -1,4 +0,0 @@
-(("Salmonella enterica"[Organism] OR ("Salmonella enterica"[Organism] 
-OR ("Salmonella enterica"[Organism] OR salmonella enterica[All Fields]))) 
-AND (cluster_public[prop] AND "biomol dna"[Properties] 
-AND "strategy wgs"[Properties]) AND "Illumina"[All Fields]) AND "USA"[All Fields]
