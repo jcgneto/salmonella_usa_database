8f9cd5c9a9682248c5b085e4af85589eaed269c2
Natasha Pavlovikj <natasha.pavlovikj@yahoo.com>
2020-02-02 14:50:27 -0600

    Add list and export
    Add list and export

--- scripts/sra_download.sh
+++ scripts/sra_download.sh
@@ -9,5 +9,10 @@ export OMP_NUM_THREADS=8
 # We use parallel-fastq-dump instead of SRAtoolkit fastq-dump because it is faster and gives less intermittent NCBI errors
 module load parallel-fastq-dump/0.6
 
-prefetch SRR9855353 && parallel-fastq-dump --sra-id SRR9855353 --threads 8 --split-files
-rm -rf SRR9855353/
+export PERL5LIB=/util/opt/anaconda/deployed-conda-envs//packages/parallel-fastq-dump/envs/parallel-fastq-dump-0.6.5/lib/site_perl/5.26.2/:/util/opt/anaconda/deployed-conda-envs/packages/parallel-fastq-dump/envs/parallel-fastq-dump-0.6.5/lib/5.26.2/:/util/opt/anaconda/deployed-conda-envs/packages/parallel-fastq-dump/envs/parallel-fastq-dump-0.6.5/lib/site_perl/5.26.2/x86_64-linux-thread-multi/:$PERL5LIB
+
+while read line
+do
+prefetch ${line} && parallel-fastq-dump --sra-id ${line} --threads 8 --split-3
+rm -rf ${line}
+done <input_sra_list.txt
